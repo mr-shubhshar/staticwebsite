d9e16bb3725507545d72c8451a9e6aeb86e1082f
Shubham Sharma <mr.shubhshar@gmail.com>
2017-05-18 22:57:55 +0530

    added: particles.js init

---
+++ app.js
@@ -0,0 +1,39 @@
+particlesJS("particles", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 500
+            }
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "size": {
+            "value": 1
+        }
+    },
+    "interactivity": {
+        "detect_on": "windows",
+        "events": {
+            "resize": true,
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+
+            }
+        },
+        "modes": {
+            "repulse": {
+                "distance": 70,
+                "duration": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
